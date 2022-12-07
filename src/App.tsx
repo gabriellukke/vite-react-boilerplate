bd15180f2d1aa9180088695b51e0074b7f0dd933
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2022-12-06 22:01:38 -0300

    chore: set airbnb eslint style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 function App() {
-
-  return (
-    <div className="text-3xl">
-	    start
-    </div>
-  )
+  return <div className="text-3xl">start</div>;
 }
 
-export default App
+export default App;
